64b7fccc33e0de50be58554c42695ada89f1742d
wagner <wagncarv@outlook.com>
2023-06-18 15:26:30 -0300

    user_utils deleted

--- lib/blog/temp_utils/user_utils.ex
+++
@@ -1,6 +0,0 @@
-defmodule Blog.TempUtils.UserUtils do
-  @moduledoc false
-  # alias Blog.Accounts.User
-
-  def print_user(user), do: %{id: user.id, email: user.email, inserted_at: user.inserted_at}
-end
